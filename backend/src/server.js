c582c5c90bb79e0ca66dc248fdb76f4ceb64af52
anik8c <anik8cgithub@gmail.com>
2026-08-09 21:24:07 +0530

    rollbacked in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,9 +23,9 @@ app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
 if (process.env.NODE_ENV === "development") {
-    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (_, res) => {
-        res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
 }
 
