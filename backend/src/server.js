703884d3e36060fea0735073884bfaf04cebd9b8
anik8c <anik8cgithub@gmail.com>
2026-08-09 14:21:50 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,28 @@
 import express from "express";
-import authRoutes from "./routes/authRoutes.js";
-import messageRoutes from "./routes/messageRoutes.js";
-import dotenv from "dotenv";
+import "dotenv/config";
 import path from "path";
 
-dotenv.config();
+import authRoutes from "./routes/authRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import { connectDB } from "./lib/db.js";
+
+
+// console.log("Environment Variables:", {
+//     PORT: process.env.PORT,
+//     MONGO_URI: process.env.MONGO_URI,
+//     NODE_ENV: process.env.NODE_ENV
+// });
 
 const app = express();
 const __dirname = path.resolve();
 
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "development") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (_, res) => {
         res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
@@ -22,5 +30,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.listen(process.env.PORT || 3000, () => {
+    connectDB();
     console.log("Server is running on PORT: " + (process.env.PORT || 3000));
 })
