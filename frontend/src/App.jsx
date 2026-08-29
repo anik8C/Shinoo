33db9ee6dd54af0605efda90ac1da45f60ae6123
anik8c <anik8cgithub@gmail.com>
2026-08-30 00:29:35 +0530

    created login page and partial logout functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <Routes>
         <Route
           path="/"
-          element={authUser ? <ChatPage /> : <Navigate to={"/signup"} />}
+          element={authUser ? <ChatPage /> : <Navigate to={"/login"} />}
         />
         <Route
           path="/signup"
