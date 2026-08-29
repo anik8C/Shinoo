621bf87ca61c72b86b0a51dff4040a42fde53073
anik8c <anik8cgithub@gmail.com>
2026-08-30 00:30:09 +0530

    created login and logout(partial)

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,145 @@
-import React from "react";
+import BorderAnimatedContainer from "../components/BorderAnimatedContainer.jsx";
+import { useAuthStore } from "../store/useAuthStore.js";
+import { useState } from "react";
+import {
+  MessageCircleIcon,
+  MailIcon,
+  LockIcon,
+  LoaderIcon,
+} from "lucide-react";
+import { Link } from "react-router";
 
 function LogInPage() {
-  return <div>LogInPage</div>;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { login, isLoggingIn } = useAuthStore();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(formData);
+  };
+
+  return (
+    <div className="w-full flex items-center justify-center p-4 bg-slate-900">
+      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]">
+        <BorderAnimatedContainer>
+          <div className="w-full flex flex-col md:flex-row">
+            {/* FORM CLOUMN - LEFT SIDE */}
+            <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30">
+              <div className="w-full max-w-md">
+                {/* HEADING TEXT */}
+                <div className="text-center mb-8">
+                  <MessageCircleIcon className="w-12 h-12 mx-auto text-slate-400 mb-4" />
+                  <h2 className="text-2xl font-bold text-slate-200 mb-2">
+                    Welcome Back
+                  </h2>
+                  <p className="text-slate-400">
+                    Login to access your account{" "}
+                  </p>
+                </div>
+
+                {/* FORM */}
+                <form onSubmit={handleSubmit} className="space-y-6">
+                  {/* EMAIL INPUT*/}
+                  <div>
+                    <label className="auth-input-label">Email</label>
+                    <div className="relative">
+                      <MailIcon className="auth-input-icon" />
+
+                      <input
+                        type="email"
+                        value={formData.email}
+                        onChange={(e) => {
+                          setFormData({
+                            ...formData,
+                            email: e.target.value,
+                          });
+                        }}
+                        className="auth-input"
+                        placeholder="sikandersingh@gmail.com"
+                      />
+                    </div>
+                  </div>
+
+                  {/* PASSWORD INPUT*/}
+                  <div>
+                    <label className="auth-input-label">Password</label>
+                    <div className="relative">
+                      <LockIcon className="auth-input-icon" />
+
+                      <input
+                        type={showPassword ? "text" : "password"}
+                        value={formData.password}
+                        onChange={(e) => {
+                          setFormData({
+                            ...formData,
+                            password: e.target.value,
+                          });
+                        }}
+                        className="auth-input"
+                        placeholder="Enter your password"
+                      />
+                      <button
+                        type="button"
+                        onClick={() => setShowPassword(!showPassword)}
+                        className="absolute right-3 top-1/2 -translate-y-1/2"
+                      >
+                        {showPassword ? "Hide" : "Show"}
+                      </button>
+                    </div>
+                  </div>
+
+                  {/* SUBMIT BUTTON */}
+                  <button
+                    type="submit"
+                    className="auth-btn"
+                    disabled={isLoggingIn}
+                  >
+                    {isLoggingIn ? (
+                      <LoaderIcon className="w-full animate-spin h-5 text-center" />
+                    ) : (
+                      "Login"
+                    )}
+                  </button>
+                </form>
+
+                <div className="mt-6 text-center">
+                  <Link to="/signup" className="auth-link">
+                    Don't have an account? Signup
+                  </Link>
+                </div>
+              </div>
+            </div>
+            {/* FORM ILLUSTRATION - RIGHT SIDE*/}
+            <div className="hidden md:w-1/2 md:flex items-center justify-center p-6 bg-gradient-to-bl from-slate-800/20 to-transparent">
+              <div>
+                <img
+                  src="/login.png"
+                  alt="shinoos chatting with each other"
+                  className="w-full h-auto object-contain"
+                />
+                <div className="mt-6 text-center">
+                  <h3 className="text-xl font-medium text-cyan-400">
+                    Connect anytime, anywhere
+                  </h3>
+
+                  <div className="mt-4 flex justify-center gap-4">
+                    <span className="auth-badge">Free</span>
+                    <span className="auth-badge">Easy Setup</span>
+                    <span className="auth-badge">Private</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </BorderAnimatedContainer>
+      </div>
+    </div>
+  );
 }
 
 export default LogInPage;
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
-
+    isLoggingIn: false,
 
     checkAuth: async () => {
         try {
@@ -35,5 +35,35 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({ isSigningUp: false });
         }
+    },
+
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("/auth/login/", data);
+            set({ authUser: res.data })
+
+            toast.success("Logged in successfully");
+        } catch (error) {
+            console.log("Error in signup: ", error);
+            toast.error(error.response.data.message);
+
+
+        } finally {
+            set({ isLoggingIn: false });
+        }
+    },
+
+    logout: async () => {
+        try {
+            axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("Logged out successfully");
+
+        } catch (error) {
+            console.log("Error in logout: ", error);
+            toast.error(error.response.data.message);
+        }
+
     }
 }));
